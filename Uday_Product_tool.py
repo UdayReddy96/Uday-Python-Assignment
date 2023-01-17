2482628750dddf24bc1ef9eefded64a652122ab6
UdayReddy96 <122855703+UdayReddy96@users.noreply.github.com>
2023-01-17 12:33:12 +0530

    Update Uday_Product_tool.py

--- Uday_Product_tool.py
+++ Uday_Product_tool.py
@@ -6,7 +6,9 @@ cnx = mysql.connector.connect(user='root', password='rps@123', host='localhost',
 cursor = cnx.cursor()
 
 # Create a new product
-def create_product(name, price):
+def create_product():
+    name = input("Enter product name: ")
+    price = float(input("Enter product price: "))
     query = "INSERT INTO products (name, price) VALUES (%s, %s)"
     values = (name, price)
     cursor.execute(query, values)
@@ -22,7 +24,10 @@ def read_products():
         print(product)
 
 # Update a product
-def update_product(id, name, price):
+def update_product():
+    id = int(input("Enter product ID: "))
+    name = input("Enter new product name: ")
+    price = float(input("Enter new product price: "))
     query = "UPDATE products SET name = %s, price = %s WHERE id = %s"
     values = (name, price, id)
     cursor.execute(query, values)
@@ -30,7 +35,8 @@ def update_product(id, name, price):
     print("Product updated successfully.")
 
 # Delete a product
-def delete_product(id):
+def delete_product():
+    id = int(input("Enter product ID: "))
     query = "DELETE FROM products WHERE id = %s"
     values = (id,)
     cursor.execute(query, values)
@@ -42,3 +48,27 @@ def close_connection():
     cursor.close()
     cnx.close()
     print("Connection closed.")
+
+# Main Menu
+while True:
+    print("1. Create Product")
+    print("2. Read Products")
+    print("3. Update Product")
+    print("4. Delete Product")
+    print("5. Exit")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        create_product()
+    elif choice == 2:
+        read_products()
+    elif choice == 3:
+        update_product()
+    elif choice == 4:
+        delete_product()
+    elif choice == 5:
+        close_connection()
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
